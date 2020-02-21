6ec992755d8e2248333d541b4aa0a8f2d20aa80a
Andy Hauser <andreas.hauser@lmu.de>
2020-02-21 21:17:40 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfastq_filter",
-    version="0.2.0",
+    version="0.3.0",
     author='Andy Hauser',
     author_email='Andreas.Hauser@LMU.de',
     license='LICENSE',
