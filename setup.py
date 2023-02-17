5921207c681e874b285dbfed317cfcf93fc8c333
Andreas Hauser <andreas.github@icloud.com>
2023-02-17 13:13:42 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfastq_filter",
-    version="0.4.0",
+    version="0.4.1",
     author='Andy Hauser',
     author_email='Andreas.Hauser@LMU.de',
     license='LICENSE',
